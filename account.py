bfea1922c773ef5978e170be59c0b6a1f851cf62
mikumino <36429067+mikumino@users.noreply.github.com>
2023-04-12 17:35:43 -0700

    Account.py
    - added __str__

--- account.py
+++ account.py
@@ -3,9 +3,10 @@ class representing a bank account; its transaction log is done through database 
 '''
 
 class Account:
-  
-  def __init__(self, accountNum, balance, card_number, pin):
-    self.accountNum = accountNum 
-    self.card_number = card_number
-    self.pin = pin
-    self.balance = balance
+    def __init__(self, accountNum, balance, card_number, pin):
+        self.accountNum = accountNum 
+        self.card_number = card_number
+        self.pin = pin
+        self.balance = balance
+    def __str__(self):
+        return f"Account ID: {self.accountNum}\nBalance: {self.balance}"
