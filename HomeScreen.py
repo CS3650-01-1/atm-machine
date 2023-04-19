a6960260feeb871c2bb70a0164f345e3905dbc42
Javila127 <61573656+Javila127@users.noreply.github.com>
2023-04-18 19:48:37 -0700

    Updated GUI to resemble storyboards (partially)

--- HomeScreen.py
+++ HomeScreen.py
@@ -1,44 +1,98 @@
 import tkinter as tk
 
-class HomeScreen:
-    def __init__(self, master):
+class HomeScreen(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
         self.master = master
-        master.title("ATM System")
+        self.create_widgets()
+
+    def create_widgets(self):
+        # Create Welcome message label
+        self.welcome_label = tk.Label(self, text="Welcome!", font=("Arial", 24))
+        self.welcome_label.pack(side="top", pady=10)
+
+        # Create Username label and input box
+        self.username_label = tk.Label(self, text="Username:")
+        self.username_label.pack(pady=5)
+        self.username_entry = tk.Entry(self)
+        self.username_entry.pack(pady=5)
+
+        # Create Password label and input box
+        self.password_label = tk.Label(self, text="Password:")
+        self.password_label.pack(pady=5)
+        self.password_entry = tk.Entry(self, show="*")
+        self.password_entry.pack(pady=5)
+
+        # Create Sign-in button
+        self.signin_button = tk.Button(self, text="Sign-in", command=self.signin_clicked)
+        self.signin_button.pack(pady=10)
+
+        # Create "Don't have an account?" message
+        self.create_account_message = tk.Label(self, text="Don't have an account?")
+        self.create_account_message.pack(pady=5)
+
+        # Create Create account button
+        self.create_account_button = tk.Button(self, text="Create account", command=self.create_account_clicked)
+        self.create_account_button.pack(pady=5)
+
+    def signin_clicked(self):
+        #Logic to be implemented later
+        print("Sign-in clicked")
+
+    def create_account_clicked(self):
+        # Switch to account creation screen
+        self.master.switch_to_account_creation_screen()
+
+class AccountCreationScreen(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.create_widgets()
+
+    def create_widgets(self):
+        # Create account creation labels and input boxes
+        self.account_creation_label = tk.Label(self, text="Create account:")
+        self.account_creation_label.pack(pady=10)
 
 
-        self.button1 = tk.Button(master, text="Check Balance", command=self.check_balance)
-        self.button1.pack()
+        self.username_label = tk.Label(self, text="Name:")
+        self.username_label.pack()
+        self.username_entry = tk.Entry(self)
+        self.username_entry.pack()
 
-        self.button2 = tk.Button(master, text="Withdraw", command=self.withdraw)
-        self.button2.pack()
+        self.phone_num_label = tk.Label(self, text="Phone number:")
+        self.phone_num_label.pack()
+        self.phone_num_entry = tk.Entry(self)
+        self.phone_num_entry.pack()
 
-        self.button3 = tk.Button(master, text="Deposit", command=self.deposit)
-        self.button3.pack()
+        self.email_label = tk.Label(self, text="Email:")
+        self.email_label.pack()
+        self.email_entry = tk.Entry(self)
+        self.email_entry.pack()
 
-        self.button4 = tk.Button(master, text="Transfer", command=self.transfer)
-        self.button4.pack()
+        self.dob_label = tk.Label(self, text="Date of birth:")
+        self.dob_label.pack()
+        self.dob_entry = tk.Entry(self)
+        self.dob_entry.pack()
 
-        self.button6 = tk.Button(master, text="Exit", command=master.quit)
-        self.button6.pack()
+        #Need to include password page
 
-    def check_balance(self):
-        self.check_balance_screen = tk.Toplevel(self.master)
-        self.check_balance_screen.title("Check Balance")
+    def create_account(self):
+        #Logic to be implemented later
+        print("Account created")
 
-    def withdraw(self):
-        self.withdraw_screen = tk.Toplevel(self.master)
-        self.withdraw_screen.title("Withdraw")
+class MainApp(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("ATM System")
+        self.geometry("800x300")
+        self.home_screen = HomeScreen(self)
+        self.account_creation_screen = AccountCreationScreen(self)
+        self.home_screen.pack()
 
-    def deposit(self):
-        self.deposit_screen = tk.Toplevel(self.master)
-        self.deposit_screen.title("Deposit")
+    def switch_to_account_creation_screen(self):
+        self.home_screen.pack_forget()
+        self.account_creation_screen.pack()
 
-    def transfer(self):
-        self.transfer_screen = tk.Toplevel(self.master)
-        self.transfer_screen.title("Transfer")
-
-
-
-root = tk.Tk()
-gui = HomeScreen(root)
-root.mainloop()
+app = MainApp()
+app.mainloop()
