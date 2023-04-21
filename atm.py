28dd63aeeedcf183b44e2f3d2574e9490ca888ba
pkflunr <71987718+pkflunr@users.noreply.github.com>
2023-04-20 22:58:14 -0700

    connect atm class to db

--- atm.py
+++ atm.py
@@ -2,12 +2,41 @@
 class representing the ATM interface; all actions that the user can do are done through this class
 '''
 from transaction import Transaction
+import sqlite3
+from sqlite3 import Error
+
 DATABASE = "atm.db"
 
 class ATM:
-    def __init__(self, location, card_available):
+    def __init__(self, atm_id, location, cash_available):
+        self.atm_id = atm_id
         self.location = location
-        self.card_available = card_available
+        self.cash_available = cash_available
+
+    def create_in_db(self):
+        connection = sqlite3.connect(DATABASE)
+        cursor = connection.cursor()
+        cursor.execute("INSERT OR ABORT INTO ATM VALUES(?, ?, ?)", (self.atm_id, self.location, self.cash_available))
+        connection.commit()
+
+    @staticmethod
+    def retrieve(atmID):
+        connection = sqlite3.connect(DATABASE)
+        cursor = connection.cursor()
+        data = cursor.execute("SELECT atmID, location, cashAvailable FROM ATM WHERE atmID = ?",[atmID]).fetchone()
+        return ATM(data[0],data[1],data[2])
+
+    def update_db(self):
+        connection = sqlite3.connect(DATABASE)
+        cursor = connection.cursor()
+        cursor.execute("INSERT OR REPLACE INTO ATM VALUES(?, ?, ?)", (self.atm_id, self.location, self.cash_available))
+        connection.commit()
+
+    def delete_from_db(self):
+        connection = sqlite3.connect(DATABASE)
+        cursor = connection.cursor()
+        cursor.execute("DELETE FROM ATM WHERE atmID = ?", [self.atm_id])
+        connection.commit()
 
     def create_transaction(self, type,amount, account_number):
         id = "0000000"  # VERY TEMP, need to figure out ID generation
