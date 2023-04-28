d2fe8251fe66db9761a954961e401ff929b432ff
mikumino <36429067+mikumino@users.noreply.github.com>
2023-04-27 17:44:31 -0700

    Transaction.py updates
    - updated to fit db changes

--- models/transaction.py
+++ models/transaction.py
@@ -7,12 +7,14 @@ DATABASE = "atm.db"
 class representing the log of a transaction
 '''
 class Transaction:
-    def __init__(self, type, amount, account, transaction_id=None, date=None):
-        self.type = type
+    def __init__(self, transactionType, amount, accountID, specificID, accountType, transaction_id=None, date=None):
+        self.transactionType = transactionType
         self.date = date or datetime.now()    # log datetime on init
         self.amount = amount
         self.transaction_id = transaction_id or self.generate_id()
-        self.account = account
+        self.accountID = accountID
+        self.specificID = specificID
+        self.accountType = accountType
 
     def generate_id(self):
         connection = sqlite3.connect(DATABASE)
@@ -27,7 +29,7 @@ class Transaction:
     def create_in_db(self):
         connection = sqlite3.connect(DATABASE)
         cursor = connection.cursor()
-        cursor.execute("INSERT OR ABORT INTO TRANSACTIONS VALUES(?, ?, ?, ?, ?)", (self.transaction_id, self.account,self.type, self.date, self.amount))
+        cursor.execute("INSERT OR ABORT INTO TRANSACTIONS VALUES(?, ?, ?, ?, ?, ?, ?)", (self.transaction_id, self.accountID, self.specificID, self.accountType, self.transactionType, self.date, self.amount))
         connection.commit()
 
     @staticmethod
@@ -36,13 +38,13 @@ class Transaction:
         cursor = connection.cursor()
         data = cursor.execute("SELECT * FROM TRANSACTIONS WHERE transactionID = ?",[transaction_id]).fetchone()
         if data:
-            return Transaction(data[2], data[4], data[1], transaction_id=data[0], date=data[3])
+            return Transaction(data[4], data[6], data[1], data[2], data[3], transaction_id=data[0], date=data[5])
         return None
     
     def update_db(self):
         connection = sqlite3.connect(DATABASE)
         cursor = connection.cursor()
-        cursor.execute("INSERT OR REPLACE INTO TRANSACTIONS VALUES (?, ?, ?, ?, ?)", (self.transaction_id, self.account, self.type, self.date, self.amount))
+        cursor.execute("INSERT OR REPLACE INTO TRANSACTIONS VALUES (?, ?, ?, ?, ?, ?, ?)", (self.transaction_id, self.accountID, self.specificID, self.accountType, self.transactionType, self.date, self.amount))
         connection.commit()
 
     def delete_from_db(self):
@@ -53,4 +55,4 @@ class Transaction:
 
     def __str__(self):  # something to print for future debugging
         # buncha objects so cant print until other str methods are done
-        return f"Type: {self.type}\nDate: {self.date}\nAmount: {self.amount}\nTransaction ID: {self.transaction_id}\nAccount ID: {self.account}"
+        return f"Type: {self.transactionType}\nDate: {self.date}\nAmount: {self.amount}\nTransaction ID: {self.transaction_id}\nAccount ID: {self.accountID}"
