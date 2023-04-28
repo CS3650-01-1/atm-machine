706c6192ae2370428e4e59546cf0527072cfcafd
ant-shen <104231150+ant-shen@users.noreply.github.com>
2023-04-28 15:51:37 -0700

    Update accountCreationController.py

--- controllers/accountCreationController.py
+++ controllers/accountCreationController.py
@@ -1,7 +1,11 @@
 import sqlite3
 from personModel import *
+from accountCreationView import *
 
 DATABASE = "atm.db"
+def submit_account(full_name, username, password, email_address, telephone_number, physical_address):
+    # Create a new Person object with the given data
+    person = Person(full_name.get(), username.get(), password.get(), email_address.get(), telephone_number.get(), physical_address.get())
 
-def submit_account():
-    print("testing")
+    # Insert the person into the database
+    person.create_in_db()
