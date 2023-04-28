e96081a0bc7f129ef00be7fa6fbff6dfbdb0f2da
mikumino <36429067+mikumino@users.noreply.github.com>
2023-04-27 17:51:38 -0700

    atm.py updates
    - updated atm methods to fit database changes

--- models/atm.py
+++ models/atm.py
@@ -48,45 +48,45 @@ class ATM:
             return True
         return False
 
-    def deposit_cash(self, amount, account_number, account_type):
+    def deposit_cash(self, amount, specific_id, account_id, account_type):
         if account_type == "checking":
-            account = Checking.retrieve(account_number)
+            account = Checking.retrieve(specific_id)
             account.addBalance(amount)
             account.update_db()
         elif account_type == "savings":
-            account = Savings.retrieve(account_number)
+            account = Savings.retrieve(specific_id)
             account.addBalance(amount)
             account.update_db()
         self.cash_available += amount
         self.update_db()
-        self.log_transaction("deposit", amount, account_number)
+        self.log_transaction("deposit", amount, specific_id, account_id, account_type)
 
     # we can probably get rid of this and just make a catch-all deposit method
-    def deposit_check(self, amount, account_number, account_type):
+    def deposit_check(self, amount, specific_id, account_id, account_type):
         if account_type == "checking":
-            account = Checking.retrieve(account_number)
+            account = Checking.retrieve(specific_id)
             account.addBalance(amount)
             account.update_db()
         elif account_type == "savings":
-            account = Savings.retrieve(account_number)
+            account = Savings.retrieve(specific_id)
             account.addBalance(amount)
             account.update_db()
-        self.log_transaction("deposit", amount, account_number)
+        self.log_transaction("deposit", amount, specific_id, account_id, account_type)
 
-    def withdraw_cash(self, amount, account_number, account_type):
+    def withdraw_cash(self, amount, specific_id, account_id, account_type):
         # deny withdrawal if not enough balance
         if account_type == "checking":
-            account = Checking.retrieve(account_number)
+            account = Checking.retrieve(specific_id)
             account.removeBalance(amount)
             account.update_db()
             self.cash_available -= amount
         elif account_type == "savings":
-            account = Savings.retrieve(account_number)
+            account = Savings.retrieve(specific_id)
             account.removeBalance(amount)
             account.update_db()
             self.cash_available -= amount
         self.update_db()
-        self.log_transaction("withdraw", amount, account_number)
+        self.log_transaction("withdraw", amount, specific_id, account_id, account_type)
 
 
     def transfer_balance(self, amount, account_number):
@@ -96,8 +96,8 @@ class ATM:
     def check_balance(self, account_number, account_type):
         pass
 
-    def log_transaction(self, type, amount, accountNum):
+    def log_transaction(self, type, amount, specific_id, account_id, account_type):
         # create transaction object and create in db
-        transaction = Transaction(type, amount, accountNum)     
+        transaction = Transaction(type, amount, account_id, specific_id, account_type)
         transaction.create_in_db()
 
--- testatm.py
+++ testatm.py
@@ -2,4 +2,4 @@ from models import atm
 
 bric = atm.ATM.retrieve(1)
 
-bric.deposit_cash(200, 19283, "savings")
+bric.deposit_cash(1, 19283, 47839, "savings")
